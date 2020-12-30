df9b160867b6bc4f421cad50a7149d06e102b773
miisscoder <misscoder@hotmail.com>
2020-12-30 18:14:23 +0800

    initial commit

---
+++ js/index.js
@@ -0,0 +1,37 @@
+﻿// video 
+document.addEventListener('DOMContentLoaded', function () {
+    function audioAutoPlay() {
+        var audio = document.getElementById('audio');
+        audio.play();
+        $('.btn-music').removeClass('pause');
+        document.addEventListener("WeixinJSBridgeReady", function () {
+            audio.play();
+        }, false);
+    }
+    audioAutoPlay();
+});
+
+
+$('.btn-music').click(function () {
+    if (!document.getElementById('audio').paused) {
+        document.getElementById('audio').pause();
+        $('.btn-music').addClass('pause');
+    } else {
+        document.getElementById('audio').play();
+        $('.btn-music').removeClass('pause');
+    }
+});
+
+
+var swiper = new Swiper('.swiper-container', {
+    direction: 'vertical',
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    }
+});
+swiper.slideTo(Number(document.cookie) ? Number(document.cookie) : 0);
+swiper.on('slideChange', function () {
+    document.cookie = swiper.activeIndex;
+});
+
